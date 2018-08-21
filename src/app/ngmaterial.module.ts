62b5393997a6bcfd9a6003486586056aacfc00c7
Gulizia, Stephanie <stephanie.gulizia@techdata.com>
2018-08-21 09:34:03 -0400

    remove spin

--- src/app/ngmaterial.module.ts
+++ src/app/ngmaterial.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatProgressSpinnerModule,  MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatButtonToggleChange, MatCardModule, MatDialogModule, MatInputModule, MatFormFieldModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatButtonToggleChange, MatCardModule, MatDialogModule, MatInputModule, MatFormFieldModule} from '@angular/material';
 import {MatButtonToggleModule} from '@angular/material/button-toggle';
 import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatCheckboxModule, MatIconModule, MatButtonToggleModule, MatMenuModule, MatCardModule, MatDialogModule, MatInputModule, MatFormFieldModule, MatProgressSpinnerModule],
-  exports: [MatButtonModule, MatToolbarModule, MatCheckboxModule, MatIconModule, MatButtonToggleModule, MatMenuModule, MatCardModule, MatDialogModule, MatInputModule, MatFormFieldModule, MatProgressSpinnerModule]
+  imports: [MatButtonModule, MatToolbarModule, MatCheckboxModule, MatIconModule, MatButtonToggleModule, MatMenuModule, MatCardModule, MatDialogModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatToolbarModule, MatCheckboxModule, MatIconModule, MatButtonToggleModule, MatMenuModule, MatCardModule, MatDialogModule, MatInputModule, MatFormFieldModule]
 })
 export class MaterialAppModule { }
